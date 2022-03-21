5e708d69a669361e996fea4372d4aaf480c6214a
Elian Arnaud <arnaud35u@etu.univ-lorraine.fr>
2022-03-21 11:50:41 +0100

    Ajout du constructeur et des getters

--- src/Position.java
+++ src/Position.java
@@ -2,4 +2,16 @@ public class Position {
 
     private int x,y;
 
+    public Position(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
