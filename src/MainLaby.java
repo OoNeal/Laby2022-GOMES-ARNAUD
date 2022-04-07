835ba98c9f939190ac4d2e436c0ef8c409605a9a
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-04-07 21:30:51 +0200

    Dernière correction sur le mainLaby

--- src/MainLaby.java
+++ src/MainLaby.java
@@ -1,42 +1,46 @@
+import src.ActionInconnueException;
 import java.util.Scanner;
 import java.io.IOException;
 
 public class MainLaby {
     public static void main(String[] args) {
         boolean end = false;
+        String nomFichier = "";
         String action = "";
+        Labyrinthe laby = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenue dans le jeu du labyrinthe !");
+        System.out.println("Quel est le nom du fichier contenant le labyrinthe ?");
+        nomFichier = sc.nextLine();
+        try {
+            laby = Labyrinthe.chargerLabyrinthe(nomFichier);
+        }
+        catch (FichierIncorrectException e2) {
+            System.out.println("Le fichier n'existe pas ");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("Erreur lors du chargement du labyrinthe");
+            System.exit(1);
+        }
         while (!end) {
-            try {
-                Labyrinthe laby = Labyrinthe.chargerLabyrinthe(args[0]);
-                System.out.println(laby.toString());
-                System.out.println("Veuillez entrer une action :");
-                Scanner sc = new Scanner(System.in);
-                action = sc.nextLine();
-                if (action.equals("exit")) {
-                    end = true;
-                } else {
-                    try {
-                        laby.deplacerPerso(action);
-                    }
-                    catch (ActionInconnueException e) {
-                        System.out.println("Action inconnue");
-                        System.out.println("Veuillez entrer une action :");
-                        action = sc.nextLine();
-                        e.printStackTrace();
-                    }
+            System.out.println(laby.toString());
+            System.out.println("Veuillez entrer une action :");
+            action = sc.nextLine();
+            if (action.equals("exit")) {
+                end = true;
+            } else {
+                try {
+                    laby.deplacerPerso(action);
                 }
-                if(laby.etreFini()){
-                    System.out.println("Vous avez réussi à finir le labyrinthe");
-                    end = true;
+                catch (ActionInconnueException e) {
+                    System.out.println("Action inconnue");
+                    System.out.println("Veuillez entrer une action :");
+                    action = sc.nextLine();
                 }
             }
-            catch (FichierIncorrectException e2) {
-                System.out.println("Le fichier n'existe pas ");
-                System.exit(0);
-            }
-            catch (IOException e) {
-                System.out.println("Erreur lors du chargement du labyrinthe");
-                System.exit(1);
+            if(laby.etreFini()){
+                System.out.println("Vous avez reussi a finir le labyrinthe");
+                end = true;
             }
         }
     }
